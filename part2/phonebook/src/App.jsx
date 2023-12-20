8586eb61efdfdfd9510156b324e8eed08e64ebc5
Dakouri M. Kobri <maurille.kobri@gmail.com>
2023-12-20 18:14:08 +0100

    Complete exercise 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,46 @@
+// NPM Packages
+import { useState } from 'react';
+
 function App() {
-  return <h1>Phonebook</h1>;
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  function addName(event) {
+    event.preventDefault();
+
+    const newNameObject = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(newNameObject));
+    setNewName('');
+  }
+
+  function handleNameChange(event) {
+    setNewName(event.target.value);
+  }
+
+  const personsList = persons.map((person) => (
+    <div key={person.name}>{person.name}</div>
+  ));
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      <div>{personsList}</div>
+    </div>
+  );
 }
 
 export default App;
