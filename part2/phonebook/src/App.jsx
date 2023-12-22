06b5a03fe03e35dc065daff19e8ddd4ae8e2fe95
Dakouri M. Kobri <maurille.kobri@gmail.com>
2023-12-22 11:23:05 +0100

    Complete exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,6 @@
 // NPM Packages
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 // Local Files
 import PersonForm from './components/PersonForm';
@@ -7,13 +8,15 @@ import Persons from './components/Persons';
 import Filter from './components/Filter';
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [searchedTerm, setSearchedTerm] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3301/persons').then((response) => {
+      const persons = response.data;
+      setPersons(persons);
+    });
+  }, []);
 
   function handleAddPerson(newPerson) {
     persons.forEach((person) => {
