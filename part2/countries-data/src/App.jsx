7805618e5b951882ae97459bd902e7ef9b8d0306
Dakouri M. Kobri <maurille.kobri@gmail.com>
2023-12-23 11:29:27 +0100

    Set up countries-data project

---
+++ part2/countries-data/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Countries Data</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ part2/countries-data/src/main.jsx
@@ -0,0 +1,7 @@
+// NPM Packages
+import ReactDOM from 'react-dom/client';
+
+// Local Files
+import App from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
